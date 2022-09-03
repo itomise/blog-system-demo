06ee30cb5532c93c2664af124f8810dee068e0c7
itomise <itomise.mizumi@gmail.com>
2022-09-03 16:21:49 +0900

    refactor structure

--- backend/src/main/kotlin/Application.kt
+++ backend/src/main/kotlin/Application.kt
@@ -3,10 +3,11 @@ package com.itomise
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.itomise.plugins.*
+import com.itomise.controller.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureSerialization()
-        configureRouting()
+        userRouting()
     }.start(wait = true)
 }
