fefe10e393ebae146e83699c4479e8201a70368b
itomise <itomise.mizumi@gmail.com>
2022-09-11 17:36:54 +0900

    fix datetime default value

--- backend/src/main/kotlin/com/itomise/infrastructure/dao/UsersDaos.kt
+++ backend/src/main/kotlin/com/itomise/infrastructure/dao/UsersDaos.kt
@@ -1,12 +1,12 @@
 package com.itomise.com.itomise.infrastructure.dao
 
 import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.javatime.timestamp
-import java.time.Instant
+import org.jetbrains.exposed.sql.javatime.CurrentDateTime
+import org.jetbrains.exposed.sql.javatime.datetime
 
 object UserDao : Table(name = "users") {
-    val createdAt = timestamp(name = "created_at").default(Instant.now())
-    val updatedAt = timestamp(name = "updated_at").default(Instant.now())
+    val createdAt = datetime(name = "created_at").defaultExpression(CurrentDateTime())
+    val updatedAt = datetime(name = "updated_at").defaultExpression(CurrentDateTime())
     val id = varchar(name = "id", length = 255).index()
     val name = varchar(name = "name", length = 255)
 
