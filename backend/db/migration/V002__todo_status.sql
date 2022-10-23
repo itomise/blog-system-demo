9b87cd88f449c0523f958fe0649723649f5ef894
itomise <itomise.mizumi@gmail.com>
2022-10-23 21:24:48 +0900

    feat: add todo_status migration

--- backend/db/migration/V002__todo_status.sql
+++ backend/db/migration/V002__todo_status.sql
@@ -1,8 +1,11 @@
 create table todo_status
 (
-    status      varchar(20),
+    status      varchar(20) not null,
 
     primary key (status)
 );
 
 grant all on todo_status to im;
+
+insert into todo_status
+values ('NEW'), ('DOING'), ('DONE');
