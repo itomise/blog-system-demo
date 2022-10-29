293816e9c7ceee54bda88232f534cc3184b3b8fb
itomise <itomise.mizumi@gmail.com>
2022-10-29 15:23:10 +0900

    fix: remove emotion

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { Global } from '@emotion/react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
-import { globalStyles } from '@/styles/globalStyles'
 import { queryClient } from '@/libs/react-query'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <QueryClientProvider client={queryClient}>
-    <Global styles={globalStyles} />
-
     <Component {...pageProps} />
   </QueryClientProvider>
 )
