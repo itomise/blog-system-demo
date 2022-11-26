62b1c872250b67b236471cd888249783e6a5fbda
itomise <itomise.mizumi@gmail.com>
2022-11-26 16:37:08 +0900

    sessionの有効期限を設定

--- backend/src/main/kotlin/com/itomise/infrastructure/RedisFactory.kt
+++ backend/src/main/kotlin/com/itomise/infrastructure/RedisFactory.kt
@@ -3,13 +3,35 @@ package com.itomise.com.itomise.infrastructure
 import io.github.crackthecodeabhi.kreds.connection.Endpoint
 import io.github.crackthecodeabhi.kreds.connection.KredsClient
 import io.github.crackthecodeabhi.kreds.connection.newClient
+import io.ktor.server.sessions.*
 
 lateinit var redisClient: KredsClient
 
 object RedisFactory {
+    val SESSION_EXPIRES_DURATION: Long = 60 * 60 * 24 * 30
     fun init(endpoint: String) {
         newClient(Endpoint.from(endpoint)).use { client ->
             redisClient = client
         }
+    }
+}
+
+class SessionStorageRedis : SessionStorage {
+    private val EXPIRES_DURATION = RedisFactory.SESSION_EXPIRES_DURATION
+    override suspend fun write(id: String, value: String) {
+        redisClient.set(id, value)
+        redisClient.expire(id, EXPIRES_DURATION.toULong())
+    }
+
+    override suspend fun read(id: String): String {
+        val session = redisClient.get(id)
+        if (session != null) {
+            redisClient.expire(id, EXPIRES_DURATION.toULong())
+            return session
+        } else throw NoSuchElementException("Session $id not found")
+    }
+
+    override suspend fun invalidate(id: String) {
+        redisClient.del(id)
     }
 }
--- backend/src/main/kotlin/com/itomise/module/Authentication.kt
+++ backend/src/main/kotlin/com/itomise/module/Authentication.kt
@@ -2,18 +2,15 @@ package com.itomise.com.itomise.module
 
 import com.itomise.com.itomise.domain.auth.UserPrincipal
 import com.itomise.com.itomise.domain.user.interfaces.IUserRepository
-import com.itomise.com.itomise.domain.user.vo.UserId
-import com.itomise.com.itomise.infrastructure.redisClient
+import com.itomise.com.itomise.infrastructure.SessionStorageRedis
 import com.itomise.com.itomise.util.getKoinInstance
 import com.itomise.getEnvConfig
-import com.itomise.infrastructure.dbQuery
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.sessions.*
 import io.ktor.util.*
-import java.util.*
 
 fun Application.authentication() {
     val isDev = environment.developmentMode
@@ -35,33 +32,13 @@ fun Application.authentication() {
     install(Authentication) {
         session<UserPrincipal>("auth-session") {
             validate { session ->
-                if (!session.id.isNullOrBlank()) {
-                    val res = dbQuery {
-                        userRepository.findByUserId(UserId(UUID.fromString(session.id)))
-                    }
-
-                    if (res != null) {
-                        UserPrincipal(id = res.id.value.toString())
-                    } else null
+                if (session.id.isNotEmpty()) {
+                    session
                 } else null
             }
             challenge {
                 call.respond(HttpStatusCode.Unauthorized)
             }
         }
-    }
-}
-
-class SessionStorageRedis : SessionStorage {
-    override suspend fun write(id: String, value: String) {
-        redisClient.set(id, value)
-    }
-
-    override suspend fun read(id: String): String {
-        return redisClient.get(id) ?: throw NoSuchElementException("Session $id not found")
-    }
-
-    override suspend fun invalidate(id: String) {
-        redisClient.del(id)
     }
 }
