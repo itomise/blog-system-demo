1f544403fbdd731364c3d126478774a5de965ace
itomise <itomise.mizumi@gmail.com>
2022-12-03 12:27:46 +0900

    共通鍵の暗号・復号クラス作成

---
+++ backend/src/test/kotlin/com/itomise/util/AesGcmCipherTest.kt
@@ -0,0 +1,20 @@
+package com.itomise.util
+
+import com.itomise.com.itomise.util.AesGcmCipher
+import kotlin.test.Test
+
+class AesGcmCipherTest {
+    @Test
+    fun test() {
+        val plainText = "This is plain text."
+        val key = "ZXp3ifkqaRiGZyvZ6HcK"
+
+        val cipher = AesGcmCipher(key)
+
+        val encryptData = cipher.encrypt(plainText)
+
+        val decryptData = cipher.decrypt(encryptData)
+
+        assert(decryptData == plainText)
+    }
+}
