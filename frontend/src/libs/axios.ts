805e86f6a8799dda07bc3f70237a05b20fc48102
itomise <itomise.mizumi@gmail.com>
2023-01-07 16:57:37 +0900

    update backend prod path

--- frontend/src/libs/axios.ts
+++ frontend/src/libs/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const appAxios = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : '/api',
   headers: {
     'X-Requested-With': 'HttpRequest',
     Accept: 'application/json',
