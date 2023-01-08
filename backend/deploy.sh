b3ac29ac8b934c44bc09029c9bd7ca4b41e67eb3
itomise <itomise.mizumi@gmail.com>
2023-01-08 19:23:02 +0900

    fix

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -9,7 +9,7 @@ gcloud run deploy \
   --args="-config=src/main/resources/application.prod.conf" \
   --update-secrets=DB_PASSWORD=db-user-password:latest,DB_USER=db-user-name:latest,INSTANCE_UNIX_SOCKET=instance-unix-socket:latest,INSTANCE_CONNECTION_NAME=instance-connection-name:latest,DB_URL=db-url:latest,JWT_PRIVATE_KEY=jwt-private-key:latest,SENDGRID_API_KEY=sendgrid-api-key:latest,SESSION_SIGN_KEY=session-sign-key:latest,JWT_ENCRYPTION_KEY=jwt-encryption-key:latest,REDIS_ENDPOINT=redis-endpoint:latest \
   backend
-  
+
 # 備忘録
 #  --vpc-connector { redisに接続するための vpc コネクタ名 } \
 #  --add-cloudsql-instances { cloud sql のインスタンスコネクション名 } \
