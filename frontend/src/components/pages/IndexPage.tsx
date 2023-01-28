bbf05276c2990c3042c54031f5ad24bed2518ea2
itomise <itomise.mizumi@gmail.com>
2023-01-28 16:44:23 +0900

    key わたし忘れ

--- frontend/src/components/pages/IndexPage.tsx
+++ frontend/src/components/pages/IndexPage.tsx
@@ -3,12 +3,14 @@ import { Card, Container, Image, Text, Title, useMantineTheme, Grid, Group, Badg
 import Link from 'next/link'
 
 const data: {
+  id: string
   image: string
   title: string
   tags: string[]
   date: string
 }[] = [
   {
+    id: '530e2756-513d-4570-af28-2ec1cbfcc1b7',
     image:
       'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80',
     title: 'タイトル',
@@ -16,6 +18,7 @@ const data: {
     date: '2023.01.28',
   },
   {
+    id: '410d3aab-6565-4291-820c-1e53ed74fd44',
     image:
       'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80',
     title: 'タイトル',
@@ -23,6 +26,7 @@ const data: {
     date: '2023.01.28',
   },
   {
+    id: 'fa0a0350-e7f6-4343-aede-37b4bb83a371',
     image:
       'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80',
     title: 'タイトル',
@@ -30,6 +34,7 @@ const data: {
     date: '2023.01.28',
   },
   {
+    id: '4f9cc4cc-0aa0-4dc4-b539-a3007ddbff38',
     image:
       'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80',
     title: 'タイトル',
@@ -60,7 +65,7 @@ export const IndexPage: React.FC = () => {
 
           <Grid mt="lg">
             {data.map((d) => (
-              <Grid.Col span={5}>
+              <Grid.Col span={5} key={d.id}>
                 <Link href="/">
                   <Card shadow="lg" p="sm" component="article">
                     <Card.Section>
@@ -72,7 +77,7 @@ export const IndexPage: React.FC = () => {
                     {d.tags.length > 0 && (
                       <Group mt="xs" spacing="xs">
                         {d.tags.map((t) => (
-                          <Badge>{t}</Badge>
+                          <Badge key={t}>{t}</Badge>
                         ))}
                       </Group>
                     )}
