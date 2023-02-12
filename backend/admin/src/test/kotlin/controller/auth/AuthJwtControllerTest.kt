b8bf19a9a3648a46c907dce96618b3ad6bc7bf82
itomise <itomise.mizumi@gmail.com>
2023-02-12 17:37:01 +0900

    add cache and mock jwks in all tests

--- backend/admin/src/test/kotlin/controller/auth/AuthJwtControllerTest.kt
+++ backend/admin/src/test/kotlin/controller/auth/AuthJwtControllerTest.kt
@@ -1,21 +1,16 @@
 package controller.auth
 
-import com.auth0.jwk.Jwk
-import com.auth0.jwk.JwkProvider
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.itomise.admin.controller.requestModels.JwtLoginResponseModel
 import com.itomise.admin.controller.requestModels.LoginRequestModel
 import com.itomise.admin.controller.responseModels.MeResponseModel
-import com.itomise.admin.module.jwkProvider
 import controller.BaseTestApplication.Companion.appTestApplication
 import controller.BaseTestApplication.Companion.authSessionUserForTest
 import controller.BaseTestApplication.Companion.cleanup
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.mockk.every
-import io.mockk.mockk
 import java.util.*
 import kotlin.test.AfterTest
 import kotlin.test.Test
@@ -29,20 +24,6 @@ internal class AuthJwtControllerTest {
 
     @Test
     fun `ログインできること`() = appTestApplication {
-        val mockJwkProvider = mockk<JwkProvider>()
-        every { mockJwkProvider.get(any()) } returns Jwk.fromValues(
-            mapOf(
-                "use" to "sig",
-                "kty" to "RSA",
-                "n" to "ql5HLRkh27pA0zONngdyk_mRo7TgcR7Et31YbqDsWDcfLYN_P1XrdTetg89HHuSC_P_G5rabx3NIzenK_Ej8lZES0F7lpagIwaMZQjPO0urEp53MuRhoogppBr6uxRP_Mkv-bESK_cTNaTgG5nfkWzjfq6Bx1wjNOhWQDONAd81V9jFt8vm0oDzdErsBKUmuysRo7Seuol2mgD5D8AyBYyv79NRaP1anuje4h0DUo45yevxOjjdyAwCcM6uZPPNtDN7AoM469il--rgV-17DJSz3lKnYUdwepqn0ULeqCPORRjJ0p-B5VDJI0_CuYMiO8XsQh43y-znq8pYiIkISow",
-                "e" to "AQAB",
-                "kid" to "673a5ea3-a4ae-422b-be55-a9c98e674550",
-                "alg" to "RS256"
-            )
-        )
-
-        jwkProvider = mockJwkProvider
-        
         val name = "テスト太郎"
         val email = "${UUID.randomUUID()}@test.test"
         val password = UUID.randomUUID().toString()
--- backend/admin/src/test/kotlin/controller/auth/SignUpInteractorTest.kt
+++ backend/admin/src/test/kotlin/controller/auth/SignUpInteractorTest.kt
@@ -1,16 +1,11 @@
 package controller.auth
 
-import com.auth0.jwk.Jwk
-import com.auth0.jwk.JwkProvider
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.itomise.admin.controller.requestModels.SignUpRequestModel
-import com.itomise.admin.module.jwkProvider
 import controller.BaseTestApplication
 import controller.BaseTestApplication.Companion.appTestApplication
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.mockk.every
-import io.mockk.mockk
 import java.util.*
 import kotlin.test.AfterTest
 import kotlin.test.Test
@@ -24,20 +19,6 @@ internal class SignUpInteractorTest {
 
     @Test
     fun `ユーザー作成できること`() = appTestApplication {
-        val mockJwkProvider = mockk<JwkProvider>()
-        every { mockJwkProvider.get(any()) } returns Jwk.fromValues(
-            mapOf(
-                "use" to "sig",
-                "kty" to "RSA",
-                "n" to "ql5HLRkh27pA0zONngdyk_mRo7TgcR7Et31YbqDsWDcfLYN_P1XrdTetg89HHuSC_P_G5rabx3NIzenK_Ej8lZES0F7lpagIwaMZQjPO0urEp53MuRhoogppBr6uxRP_Mkv-bESK_cTNaTgG5nfkWzjfq6Bx1wjNOhWQDONAd81V9jFt8vm0oDzdErsBKUmuysRo7Seuol2mgD5D8AyBYyv79NRaP1anuje4h0DUo45yevxOjjdyAwCcM6uZPPNtDN7AoM469il--rgV-17DJSz3lKnYUdwepqn0ULeqCPORRjJ0p-B5VDJI0_CuYMiO8XsQh43y-znq8pYiIkISow",
-                "e" to "AQAB",
-                "kid" to "673a5ea3-a4ae-422b-be55-a9c98e674550",
-                "alg" to "RS256"
-            )
-        )
-
-        jwkProvider = mockJwkProvider
-        
         client.post("/api/auth/sign-up") {
             contentType(ContentType.Application.Json)
             setBody(
