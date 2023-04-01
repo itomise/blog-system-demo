a38ce3a2b002e8b1cadb91125693569c920f12ed
itomise <itomise.mizumi@gmail.com>
2023-04-01 11:49:33 +0900

    fix dependencies

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   reactStrictMode: true,
   compiler: {
     emotion: true,
-    removeConsole: process.env.NODE_ENV === 'production',
   },
   output: 'standalone',
 }
